e2b1a02b0747169d8b3d4ec6a1e96d13f253abff
dionisis2014 <toulatosdionisis@gmail.com>
2022-11-19 05:34:47 +0200

    Added base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://dionisis2014.github.io'
+  site: 'https://dionisis2014.github.io',
+  base: '/incident-counter'
 });
